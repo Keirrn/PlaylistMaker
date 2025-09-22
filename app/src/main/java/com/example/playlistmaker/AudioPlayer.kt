b2c5ec2a8a5adc4bb94a3a770e0847ec246a9a7f
Your Name <you@example.com>
2025-09-22 23:40:09 +0300

    commit

--- app/src/main/java/com/example/playlistmaker/AudioPlayer.kt
+++ app/src/main/java/com/example/playlistmaker/AudioPlayer.kt
@@ -1,8 +1,10 @@
 package com.example.playlistmaker
 
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -18,7 +20,23 @@ import com.google.android.material.appbar.MaterialToolbar
 import com.google.gson.Gson
 
 class AudioPlayer : AppCompatActivity() {
+
+    companion object {
+        const val TRACK_JSON_KEY = "trackJson"
+        private const val STATE_DEFAULT = 0
+        private const val STATE_PREPARED = 1
+        private const val STATE_PLAYING = 2
+        private const val STATE_PAUSED = 3
+    }
+
+    private var playerState = STATE_DEFAULT
+
+    private lateinit var mediaPlayer : MediaPlayer
+    private lateinit var playButton: ImageButton
+    private lateinit var url: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_audio_player)
@@ -29,19 +47,22 @@ class AudioPlayer : AppCompatActivity() {
                 top = systemBars.top,
                 bottom = systemBars.bottom
             )
-
             insets
         }
+
         val track = if (savedInstanceState != null) {
             val trackJson = savedInstanceState.getString(TRACK_JSON_KEY)
             Gson().fromJson(trackJson, Track::class.java)
         } else {
             Gson().fromJson(intent.getStringExtra(TRACK_JSON_KEY), Track::class.java)
         }
+
+
         val backBar = findViewById<MaterialToolbar>(R.id.backbar)
         backBar.setNavigationOnClickListener {
             finish()
         }
+        url = track.previewUrl
         val cover = findViewById<ImageView>(R.id.album_cover)
         val song = findViewById<TextView>(R.id.song)
         val singer = findViewById<TextView>(R.id.singer)
@@ -52,13 +73,13 @@ class AudioPlayer : AppCompatActivity() {
         val genre = findViewById<TextView>(R.id.track_genre)
         val country = findViewById<TextView>(R.id.track_country)
         val duration = findViewById<TextView>(R.id.full_time)
+
         song.text = track.trackName
         singer.text = track.artistName
         collection.text = track.collectionName ?: ""
         year.text = track.releaseDate?.substring(0,4) ?: ""
         genre.text = track.primaryGenreName
         country.text = track.country
-
         duration.text = formatMillis(track.trackTimeMillis)
         if (track.collectionName == null){
             collection.visibility = View.GONE
@@ -76,15 +97,65 @@ class AudioPlayer : AppCompatActivity() {
             year.visibility = View.VISIBLE
             year_nc.visibility = View.VISIBLE
         }
+
         Glide.with(this)
             .load(track.artworkUrl100.replaceAfterLast("/", "512x512bb.jpg"))
             .placeholder(R.drawable.placeholder_audioplayer)
             .centerCrop()
             .transform(RoundedCorners(dpToPx(8f, this)))
             .into(cover)
-    }
-    companion object {
-        const val TRACK_JSON_KEY = "trackJson"
+        mediaPlayer= MediaPlayer()
+        playButton = findViewById(R.id.play_btn)
+        playButton.isEnabled = false
+        preparePlayer()
+        playButton.setOnClickListener {
+            playbackControl()
+        }
     }
 
+
+    private fun preparePlayer() {
+        mediaPlayer.setDataSource(url)
+        mediaPlayer.prepareAsync()
+        mediaPlayer.setOnPreparedListener {
+            playButton.isEnabled = true
+            playerState = STATE_PREPARED
+        }
+        mediaPlayer.setOnCompletionListener {
+            playButton.setImageResource(R.drawable.play_song_ic)
+            playerState = STATE_PREPARED
+        }
+    }
+
+    private fun startPlayer() {
+        mediaPlayer.start()
+        playButton.setImageResource(R.drawable.stop_song_ic)
+        playerState = STATE_PLAYING
+    }
+    private fun pausePlayer() {
+        mediaPlayer.pause()
+        playButton.setImageResource(R.drawable.play_song_ic)
+        playerState = STATE_PAUSED
+    }
+
+    private fun playbackControl() {
+        when(playerState) {
+            STATE_PLAYING -> {
+                pausePlayer()
+            }
+            STATE_PREPARED, STATE_PAUSED -> {
+                startPlayer()
+            }
+        }
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        pausePlayer()
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
+    }
 }
