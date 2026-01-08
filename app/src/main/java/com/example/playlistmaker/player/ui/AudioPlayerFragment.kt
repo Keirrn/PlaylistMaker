e34f94025bbdc61a5020183d04f9fe5c5fd8f5a8
Your Name <you@example.com>
2026-01-08 03:15:10 +0300

    Player to fragment

--- app/src/main/java/com/example/playlistmaker/player/ui/AudioPlayerFragment.kt
+++ app/src/main/java/com/example/playlistmaker/player/ui/AudioPlayerFragment.kt
@@ -1,54 +1,46 @@
 package com.example.playlistmaker.player.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.updatePadding
-import com.example.playlistmaker.utill.TRACK
-import com.example.playlistmaker.databinding.ActivityAudioPlayerBinding
-import com.example.playlistmaker.player.domain.FormatMillisUseCase
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import com.example.playlistmaker.databinding.FragmentAudioPlayerBinding
 import com.example.playlistmaker.player.domain.ImageLoadRepository
 import com.example.playlistmaker.search.domain.Track
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
 
-class AudioPlayer : AppCompatActivity() {
+class AudioPlayerFragment : Fragment() {
 
-    private lateinit var binding: ActivityAudioPlayerBinding
-    private val imageLoadRepository : ImageLoadRepository by inject ()
-    private val viewModel: AudioPlayerViewModel by viewModel{
-        parametersOf( track?.previewUrl ?: "")
+    companion object {
+        const val ARGS_TRACK = "track"
+
+        fun createArgs(track: Track): Bundle =
+            bundleOf(ARGS_TRACK to track)
+    }
+
+    private lateinit var binding: FragmentAudioPlayerBinding
+    private val imageLoadRepository: ImageLoadRepository by inject()
+    private val viewModel: AudioPlayerViewModel by viewModel {
+        parametersOf(track?.previewUrl ?: "")
     }
 
     private lateinit var track: Track
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAudioPlayerBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityAudioPlayerBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        enableEdgeToEdge()
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-
-
-        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { view, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            view.updatePadding(
-                top = systemBars.top,
-                bottom = systemBars.bottom
-            )
-            insets
-        }
-
-        track = intent.getParcelableExtra<Track>(TRACK)!!
-        binding.backbar.setNavigationOnClickListener {
-            finish()
-        }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        track = requireArguments().getParcelable<Track>(ARGS_TRACK)!!
 
         binding.song.text = track.trackName
         binding.singer.text = track.artistName
@@ -59,19 +51,19 @@ class AudioPlayer : AppCompatActivity() {
         binding.fullTime.text = track.trackTime
 
         if (track.collectionName == null) {
-            binding.collectionName.visibility = android.view.View.GONE
-            binding.collection.visibility = android.view.View.GONE
+            binding.collectionName.visibility = View.GONE
+            binding.collection.visibility = View.GONE
         } else {
-            binding.collectionName.visibility = android.view.View.VISIBLE
-            binding.collection.visibility = android.view.View.VISIBLE
+            binding.collectionName.visibility = View.VISIBLE
+            binding.collection.visibility = View.VISIBLE
         }
 
         if (track.releaseDate == null) {
-            binding.trackYear.visibility = android.view.View.GONE
-            binding.year.visibility = android.view.View.GONE
+            binding.trackYear.visibility = View.GONE
+            binding.year.visibility = View.GONE
         } else {
-            binding.trackYear.visibility = android.view.View.VISIBLE
-            binding.year.visibility = android.view.View.VISIBLE
+            binding.trackYear.visibility = View.VISIBLE
+            binding.year.visibility = View.VISIBLE
         }
 
         imageLoadRepository.loadImage(
@@ -106,14 +98,17 @@ class AudioPlayer : AppCompatActivity() {
                 binding.playBtn.isEnabled = false
                 binding.playBtn.setImageResource(com.example.playlistmaker.R.drawable.play_song_ic)
             }
+
             AudioPlayerViewModel.STATE_PREPARED -> {
                 binding.playBtn.isEnabled = true
                 binding.playBtn.setImageResource(com.example.playlistmaker.R.drawable.play_song_ic)
             }
+
             AudioPlayerViewModel.STATE_PLAYING -> {
                 binding.playBtn.isEnabled = true
                 binding.playBtn.setImageResource(com.example.playlistmaker.R.drawable.stop_song_ic)
             }
+
             AudioPlayerViewModel.STATE_PAUSED -> {
                 binding.playBtn.isEnabled = true
                 binding.playBtn.setImageResource(com.example.playlistmaker.R.drawable.play_song_ic)
