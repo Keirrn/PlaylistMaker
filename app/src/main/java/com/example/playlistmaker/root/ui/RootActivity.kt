ec4a387532b40db6e91111eda9c4829e008ad401
Your Name <you@example.com>
2026-01-08 10:22:57 +0300

    fix visibility navigationBar

--- app/src/main/java/com/example/playlistmaker/root/ui/RootActivity.kt
+++ app/src/main/java/com/example/playlistmaker/root/ui/RootActivity.kt
@@ -1,6 +1,7 @@
 package com.example.playlistmaker.root.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -29,5 +30,14 @@ class RootActivity : AppCompatActivity()  {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bottomNavigationView.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.audioPlayerFragment -> {
+                    binding.bottomNavigationView.visibility = View.GONE
+                }
+                else -> {
+                    binding.bottomNavigationView.visibility = View.VISIBLE
+                }
+            }
     }
-}
+}}
